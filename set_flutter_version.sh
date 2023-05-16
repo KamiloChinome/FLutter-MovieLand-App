7c1c54b4a1ec2472e8a02fc854aad64e22cb2d7e
Kamilo Chinome <kamiloandresch@gmail.com>
2023-05-16 10:36:46 -0500

    set_flutter_version.sh aggregate

---
+++ set_flutter_version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "⚙️ INITIALIZE ⚙️"
+FLUTTER_DIRECTORY="C:/flutter"
+FLUTTER_VERSION="3.10.0"
+CURRENT_DIR=$(pwd)
+cd $FLUTTER_DIRECTORY
+git checkout  $FLUTTER_VERSION 
+$FLUTTER_DIRECTORY/bin/flutter --version
+cd $CURRENT_DIR
+$FLUTTER_DIRECTORY/bin/flutter pub get
+
+echo "⚡ TO COMPLETE, YOU MUST RESTART VSCODE ⚡"
+echo "⚡ PRESS CMD + SHIT + P AND RESET WINDOWS OR RELOAD WINDOWS⚡"
+echo "⚡ FINISH ⚡"
